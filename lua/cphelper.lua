4b4308752817f4f25317272351b68156c54fed02
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2021-02-21 12:32:23 +0530

    lua-http again

--- lua/cphelper.lua
+++ lua/cphelper.lua
@@ -1,29 +1,3 @@
---local http = require "http"
-local pegasus = require "pegasus"
-local servers = {}
+local http = require "http"
 local M = {}
-function M.listen()
-  local server = pegasus:new({port = "27121"})
-  server:start(
-    function(request, response)
-      local headers = request:headers()
-      local method = request:method()
-      if method == "POST" then
-        length = tonumber(headers["Content-Length"])
-        vim.api.nvim_err_writeln("Content-Length: " .. length)
-        if length > 0 then
-          data = request:receiveBody()
-          print("Read " .. #data .. " bytes.")
-        end
-      end
-      response:write(data)
-    end
-  )
-  table.insert(servers, server)
-end
-function M.killall()
-  for i in servers do
-    i:stop()
-  end
-end
 return M
