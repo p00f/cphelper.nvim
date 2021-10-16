b832c36e924d3cfc66380892ec7e421af957fd1d
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2021-10-16 08:24:12 +0530

    Add java and kotlin

--- lua/cphelper/definitions.lua
+++ lua/cphelper/definitions.lua
@@ -4,19 +4,24 @@ return {
         c = "gcc solution.c -o c.out",
         cpp = "g++ solution.cpp -o cpp.out",
         rust = "rustc solution.rs -o rust.out",
+        kotlin = "kotlinc solution.kt -include-runtime -d solution.jar",
     },
     run_cmd = {
         c = [[.]] .. sep .. [[c.out]],
         cpp = [[.]] .. sep .. [[cpp.out]],
+        java = "java solution.java",
         lua = "lua solution.lua",
         python = "python solution.py",
-        rust = [[.]] .. sep .. [[rust.out]]
+        rust = [[.]] .. sep .. [[rust.out]],
+        kotlin = "java -jar solution.jar",
     },
     extensions = {
         c = "c",
         cpp = "cpp",
+        java = "java",
         python = "py",
         lua = "lua",
         rust = "rs",
+        kotlin = "kt",
     },
 }
